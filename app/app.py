ecbc1b4f4f071930a9dbe661e4d74453dcab42e6
zsliwa <160980048+zsliwa@users.noreply.github.com>
2024-11-20 17:12:37 +0100

    Added APScheduler (scrape_and_save once a day)

--- app/app.py
+++ app/app.py
@@ -1,20 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from flask_apscheduler import APScheduler
 from blueprints.main.routes import main_bp
 from blueprints.economy.routes import economy_bp
-
 from blueprints.sport.routes import sport_bp
 from blueprints.weather.routes import weather_bp
 
 db = SQLAlchemy()
-
-# routes wykorzystuje models.py, które pobiera db z app, więc przeniosłam to tu na razie tak prowizorycznie żeby uniknąć błędów importu
-from blueprints.news.routes import news_bp
+scheduler=APScheduler()
 
 # Initialize the app
 app = Flask(__name__)
 Bootstrap(app)
+
+# routes wykorzystuje models.py, które pobiera db z app, więc przeniosłam to tu na razie tak prowizorycznie żeby uniknąć błędów importu
+# rozwiązaniem mogłoby też chyba być przeniesienie deklaracji bazy danych do odzielnego pliku
+from blueprints.news.routes import news_bp
 
 from config import Config
 app.config.from_object(Config)
@@ -27,10 +29,21 @@ app.register_blueprint(weather_bp, url_prefix='/weather')
 
 # Initailize the database
 db.init_app(app)
+scheduler.init_app(app)
+
+from blueprints.news.data_scraper import scrape_and_save
+scheduler.add_job(
+    id='1',
+    func=scrape_and_save,
+    trigger='cron',
+    hour=0,
+    minute=0
+)
 
 
 if __name__ == '__main__':
     with app.app_context():
+        scheduler.start()
         db.create_all()
     app.run(debug=True)
 
