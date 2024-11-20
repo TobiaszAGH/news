08d52503d4e51836cc565452afed64145e59926c
zsliwa <160980048+zsliwa@users.noreply.github.com>
2024-11-20 17:17:01 +0100

    Update requirements

--- app/blueprints/news/data_scraper.py
+++ app/blueprints/news/data_scraper.py
@@ -3,83 +3,85 @@ from .models import CrimeNews, CrimeImages
 from datetime import datetime
 from bs4 import BeautifulSoup
 import time
-from app import db
+from app import db, app
 
 base_url = "https://krakow.policja.gov.pl"
 url = "https://krakow.policja.gov.pl/kr1/aktualnosci"
 
 response = requests.get(url)
 
-def data_scrape():
-    if response.status_code == 200:
-        
-        soup = BeautifulSoup(response.text, 'html.parser')
-        
-        articles = soup.find_all('li', class_='news')
+def scrape_and_save():
+    with app.app_context(): 
+        if response.status_code == 200:
+            
+            soup = BeautifulSoup(response.text, 'html.parser')
+            
+            articles = soup.find_all('li', class_='news')
 
-        for article in articles:
-            
-            link = article.find('a')['href']
-            
-            if link.startswith("/"):
+            for article in articles:
                 
-                title = article.find('strong').get_text(strip=True)
+                link = article.find('a')['href']
                 
-                existing_news = CrimeNews.query.filter_by(title=title).first()
-                if existing_news:
-                    print(f"Article '{title}' had been already saved.")
-                    continue
-                
-                summary = article.find('p').get_text(strip=True)
-                
-                image = article.find('img')['src']
-                image_src = base_url + image
-                
-                date = article.find('span', class_='data').get_text(strip=True).replace("Dodano: ", "").strip()
-                
-                full_link = base_url + link
-                
-                timestamp = datetime.strptime(date, "%d.%m.%Y")
-                
-                
-                response2 = requests.get(full_link)
-                soup2 = BeautifulSoup(response2.text, 'html.parser')
-                paragraphs = soup2.find_all('p')
-                description = paragraphs[3].get_text(strip=True)
-                full_text = summary + '\n' + description
-                
-                            
-                news_obj = CrimeNews(
-                    title=title,
-                    summary=summary,
-                    full_text=full_text,
-                    image_url=image_src,
-                    article_link=full_link,
-                    timestamp=timestamp
-                )
-                db.session.add(news_obj)
-                
-                
-                images = soup2.find_all('img')
-                for img in images:
-                    img_url = img.get('src')
-                    img_url = base_url + img_url
+                if link.startswith("/"):
                     
-                    img_obj= CrimeImages(
-                        image_url=img_url,
-                        news_id=existing_news.id
+                    title = article.find('strong').get_text(strip=True)
+                    
+                    existing_news = CrimeNews.query.filter_by(title=title).first()
+                    if existing_news:
+                        print(f"Article '{title}' had been already saved.")
+                        continue
+                    
+                    summary = article.find('p').get_text(strip=True)
+                    
+                    image = article.find('img')['src']
+                    image_src = base_url + image
+                    
+                    date = article.find('span', class_='data').get_text(strip=True).replace("Dodano: ", "").strip()
+                    
+                    full_link = base_url + link
+                    
+                    timestamp = datetime.strptime(date, "%d.%m.%Y")
+                    
+                    
+                    response2 = requests.get(full_link)
+                    soup2 = BeautifulSoup(response2.text, 'html.parser')
+                    paragraphs = soup2.find_all('p')
+                    description = paragraphs[3].get_text(strip=True)
+                    full_text = summary + '\n' + description
+                    
+                        
+                    news_obj = CrimeNews(
+                        title=title,
+                        summary=summary,
+                        full_text=full_text,
+                        image_url=image_src,
+                        article_link=full_link,
+                        timestamp=timestamp
                     )
-                    db.session.add(img_obj)         
-                
-                
-                try:
+                    db.session.add(news_obj)
                     db.session.commit()
-                    print(f"Article '{title}' saved to database.")
-                except Exception as e:
-                    db.session.rollback()
-                    print(f"Error during saving news: '{title}' to database: {e}")
+                    
+                    
+                    images = soup2.find_all('img')
+                    for img in images:
+                        img_url = img.get('src')
+                        img_url = base_url + img_url
+                        
+                        img_obj= CrimeImages(
+                            image_url=img_url,
+                            news_id=news_obj.id
+                        )
+                        db.session.add(img_obj)         
+                    
+                    
+                    try:
+                        db.session.commit()
+                        print(f"Images from news: '{title}' saved to database.")
+                    except Exception as e:
+                        db.session.rollback()
+                        print(f"Error during saving images for news: '{title}' to database: {e}")
 
-                time.sleep(5)
-                
-    else:
-        print("Connection failed.", response.status_code)
+                    time.sleep(2)
+                    
+        else:
+            print("Connection failed.", response.status_code)
