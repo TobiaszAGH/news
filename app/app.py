f4e0a72f0ba7a993730e88e2061ea162512cfbce
Józefina Młynarska <fina@MacBook-Air-Jozefina.local>
2024-11-25 16:18:51 +0100

    fixed circular import in news module

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+
 from flask_bootstrap import Bootstrap
 from flask_apscheduler import APScheduler
 from blueprints.main.routes import main_bp
@@ -7,7 +7,7 @@ from blueprints.economy.routes import economy_bp
 from blueprints.sport.routes import sport_bp
 from blueprints.weather.routes import weather_bp
 
-db = SQLAlchemy()
+
 scheduler=APScheduler()
 
 # Initialize the app
@@ -17,8 +17,9 @@ Bootstrap(app)
 # routes wykorzystuje models.py, które pobiera db, app z app.py, więc przeniosłam to tu na razie tak prowizorycznie żeby uniknąć błędów importu
 from blueprints.news.routes import news_bp
 
-from config import Config
+from config import Config, db
 app.config.from_object(Config)
+
 
 app.register_blueprint(main_bp, url_prefix='/') 
 app.register_blueprint(economy_bp, url_prefix='/economy')
@@ -30,19 +31,20 @@ app.register_blueprint(weather_bp, url_prefix='/weather')
 db.init_app(app)
 scheduler.init_app(app)
 
-from blueprints.news.data_scraper import scrape_and_save
-scheduler.add_job(
-    id='1',
-    func=scrape_and_save,
-    trigger='cron',
-    hour=0,
-    minute=0
-)
+
 
 
 if __name__ == '__main__':
     with app.app_context():
         scheduler.start()
         db.create_all()
+    from blueprints.news.data_scraper import scrape_and_save
+    scheduler.add_job(
+        id='1',
+        func=scrape_and_save,
+        trigger='cron',
+        hour=0,
+        minute=0)
+        
     app.run(debug=True)
 
--- app/config.py
+++ app/config.py
@@ -1,3 +1,6 @@
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
 class Config(object):
     SECRET_KEY = 'sridzajarwanapurakotte'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///users.db'
