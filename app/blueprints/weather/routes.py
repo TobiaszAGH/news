3a9640682ff03cd534e0f18a92ed1c76a69e30ea
Daniel Krol <danielkrol@student.agh.edu.pl>
2024-12-12 17:34:36 +0100

    little changes

--- app/blueprints/weather/routes.py
+++ app/blueprints/weather/routes.py
@@ -40,11 +40,12 @@ def hello():
     if len(cities) == 0:
         cities.append('Kraków')
     for city in cities:
-        data.append(getCurrentWeather(city, None))
-        forecast.append(getForecast(city, None))
+        if getCurrentWeather(city, None) is not None:
+            data.append(getCurrentWeather(city, None))
+            forecast.append(getForecast(city, None))
         i += 1
       
-    return render_template('weather_page.html', data = data, forecast = forecast)
+    return render_template('weather_test.html', data = data, forecast = forecast)
 
 @weather_bp.route('/widget')
 def showWidget():
