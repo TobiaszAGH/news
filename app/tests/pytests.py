afb791386a9af79720bfd380288f7c794c97ac81
Tobiasz Salik <tobiasz@student.agh.edu.pl>
2024-12-12 20:54:58 +0100

    fixed pytests.py imports

--- app/tests/pytests.py
+++ app/tests/pytests.py
@@ -5,8 +5,8 @@ import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from graph_test import *
-from weather_test import *
+from .graph_test import *
+from .weather_test import *
 
 
 
