a8cd7abbceacd7fa66576b02179dd8b324f07fcf
Antoni Zaremba <59770761+LeBulbasaur@users.noreply.github.com>
2024-12-16 10:27:04 +0100

    changes

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+
+__all__ = ['blueprints', 'data_visualization', 'app_setup']
+from .blueprints import *
+from .app_setup import app
+from .data_visualization import *
+from .config import *
+                       
