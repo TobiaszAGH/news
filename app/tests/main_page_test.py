afb3079caed10ed695ffeb5c37022f13875ab4e2
Antoni Zaremba <59770761+LeBulbasaur@users.noreply.github.com>
2024-12-16 10:35:33 +0100

    added main page pytest

---
+++ app/tests/main_page_test.py
@@ -0,0 +1,22 @@
+import pytest
+import requests_mock
+from app import app as original_app
+
+
+@pytest.fixture
+def client():
+    app = original_app
+    app.config['TESTING'] = True
+    with app.app_context():
+        with app.test_client() as client:
+            yield client
+
+@pytest.fixture
+def mocked_responses():
+    with requests_mock.Mocker() as m:
+        yield m
+
+def test_home_page(client):
+    with original_app.test_request_context():
+        response = client.get('/')
+        assert response.status_code == 200
