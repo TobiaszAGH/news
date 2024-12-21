75d2ad05b4f749d511a23c4d200d203e2d80a641
Klaudia Szostek <kszostek@student.agh.edu.pl>
2024-12-22 00:21:34 +0100

    Sport db fix

--- app/run.py
+++ app/run.py
@@ -1,5 +1,6 @@
 from app import app, db, scheduler
 from blueprints.news.data_scraper import scrape_and_save
+from blueprints.sport.routes import fetch_and_save_articles
 
 # Start the app
 def main():
@@ -10,6 +11,13 @@ def main():
     scheduler.add_job(
         id='1',
         func=scrape_and_save,
+        trigger='cron',
+        hour=0,
+        minute=0)
+    fetch_and_save_articles
+    scheduler.add_job(
+        id='fetch_sport_articles',
+        func=fetch_and_save_articles,
         trigger='cron',
         hour=0,
         minute=0)
