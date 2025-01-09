9d062f353c2b08ebd95e1dcafc2474aca0733f01
Antoni Zaremba <59770761+LeBulbasaur@users.noreply.github.com>
2025-01-09 16:45:46 +0100

    added tests

--- app/tests/main_page_test.py
+++ app/tests/main_page_test.py
@@ -21,6 +21,26 @@ def test_home_page(client):
         response = client.get('/')
         assert response.status_code == 200
 
+def test_weather_page(client):
+    with original_app.test_request_context():
+        response = client.get('/weather')
+        assert response.status_code == 200
+
+def test_economy_page(client):
+    with original_app.test_request_context():
+        response = client.get('/economy')
+        assert response.status_code == 200
+
+def test_news_page(client):
+    with original_app.test_request_context():
+        response = client.get('/news')
+        assert response.status_code == 200
+
+def test_sport_page(client):
+    with original_app.test_request_context():
+        response = client.get('/sport')
+        assert response.status_code == 200
+
 def test_404_page(client):
     with original_app.test_request_context():
         response = client.get('/nonexistent')
